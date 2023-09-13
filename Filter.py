a30b76626422142b2bceeb0e07919433b3df1e07
Noam Diamant <noam.diamant@arm.com>
2023-09-13 17:45:03 +0300

    changes the library name

--- Filter.py
+++ Filter.py
@@ -1,5 +1,5 @@
 #Please note that you have to first install the Pypdf2 library, by typing "pip install PyPDF2" in the Command prompt. This will install the library in your computer.
 
-import PyPDF2 as PDFlib
+import PyPDF2 as pdf
 welcome_message = "Hello, and welcome to the Candidates Filter!\nThis Filter you in choosing the right candidates to the position you are looking for.\nThe filter includes pre defined optoins in order to make your work easier."
 print(welcome_message)
